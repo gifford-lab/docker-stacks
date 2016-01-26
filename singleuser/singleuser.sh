86888670172495fabf326514a17ff30567628e2a
Matt Edwards <mttdwrds@gmail.com>
2016-01-26 18:26:55 -0500

    Fixed notebooks target directory.

--- singleuser/singleuser.sh
+++ singleuser/singleuser.sh
@@ -7,4 +7,5 @@ exec jupyterhub-singleuser \
   --cookie-name=$JPY_COOKIE_NAME \
   --base-url=$JPY_BASE_URL \
   --hub-prefix=$JPY_HUB_PREFIX \
-  --hub-api-url=$JPY_HUB_API_URL
+  --hub-api-url=$JPY_HUB_API_URL \
+  --notebook-dir='~/notebooks'
